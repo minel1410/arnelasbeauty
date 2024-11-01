28a31a8efd30f0cda121ad5414b617727698eea6
minel1410 <msalihagic@hotmail.com>
2024-11-01 23:54:08 +0100

    Add basic SEO

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -1,4 +1,3 @@
-
 import nodemailer from "nodemailer";
 
 require("dotenv").config();
@@ -9,7 +8,6 @@ export async function POST(request) {
     const { name, phone, email, message } = body;
 
     console.log({ name, phone, email, message });
-    //ovdje ide logika
 
     let transporter = nodemailer.createTransport({
       service: "gmail",
@@ -22,13 +20,38 @@ export async function POST(request) {
       },
     });
 
-  
+    // HTML šablon za email
+    const htmlContent = `
+  <div style="font-family: Arial, sans-serif; line-height: 1.6; background-color: #f9f9f9; padding: 20px; border-radius: 8px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);">
+    <div style="background-color: #f06d6d; color: white; padding: 10px; border-radius: 8px 8px 0 0; text-align: center;">
+      <h1>Nova rezervacija</h1>
+    </div>
+    <div style="margin: 20px 0;">
+      <p>Poštovani,</p>
+      <p>Primili ste novu rezervaciju. Ovdje su detalji:</p>
+      <ul style="list-style-type: none; padding: 0;">
+        <li><strong>Ime klijenta:</strong> ${name}</li>
+        <li><strong>Telefon:</strong> ${phone}</li>
+        <li><strong>Email:</strong> ${email}</li>
+        <li><strong>Poruka:</strong></li>
+        <p style="margin: 0; padding: 0;">${message}</p>
+      </ul>
+      <p>Hvala što ste nas odabrali!</p>
+    </div>
+    <div style="text-align: center; font-size: 12px; color: #777; margin-top: 20px;">
+      <p>Za dodatne informacije, slobodno nas kontaktirajte.</p>
+      <p>Srdačan pozdrav,<br>Beauty Studio by Arnela</p>
+    </div>
+  </div>
+`;
+
+
     // Slanje emaila
     await transporter.sendMail({
       from: '"Beauty Studio" <beautystudiobyarnela@gmail.com>', // pošiljatelj
       to: "minelsalihagic@gmail.com", // tvoj email (gdje ćeš primati poruke)
       subject: `New message from ${name}`, // predmet emaila
-      text: `Phone: ${phone}\nEmail: ${email}\nMessage: ${message}`, // tekst emaila
+      html: htmlContent, // HTML sadržaj emaila
     });
 
     return new Response(
@@ -38,7 +61,7 @@ export async function POST(request) {
       }
     );
   } catch (error) {
-    console.log("Error: ", error)
+    console.log("Error: ", error);
     return new Response(
       JSON.stringify({ success: false, error: "Message failed to send." }),
       {
